5214f831e8460044c9089a2e8407ef765bc55ffc
Bill Hanson <doubleyuhtee@gmail.com>
2020-12-03 17:04:39 -0600

    bad day 2 just for fun

--- day2.py
+++ day2.py
@@ -1,7 +1,17 @@
+import re
 import requests
 
 if __name__ == "__main__":
     headers = {"Cookie": "session=" + str(open("sessioncookie").readline())}
     resp = requests.get("https://adventofcode.com/2020/day/2/input", headers=headers)
     body = resp.content.decode()
-    print(body)
+
+    count = 0
+    for line in body.splitlines():
+        split = re.split('-| |:', line)
+        print(line)
+        if re.match("(.*" + split[2] + ".*){" + split[0] + "}", split[4]) \
+                and not re.match("(.*" + split[2] + ".*){" + split[1] + "}", split[4]):
+            count += 1
+    print(count)
+    # Slow, ugly, doesn't work.  Just for posterity
