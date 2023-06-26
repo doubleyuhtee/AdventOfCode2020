3dce82b9d8d6297d8e8b4351ec3343d606405cc6
Bill Hanson <42045551+doubleyuhtee@users.noreply.github.com>
2023-06-26 16:27:42 -0500

    Save some old code in a random place

--- day2.py
+++ day2.py
@@ -1,6 +1,14 @@
 import re
 import requests
 
+YES = 1
+
+def is_palendrome(x:str, r:str = None, i:int = -1):
+    return bool(p(x, r=None, i = 0)) if i == -1 else p(x, x[::-1], 0) if r is None else YES if x == "" else p(x[1:], r=r, i=i) if x[0].isspace() else p(x, r=r, i=i+1) if r[i].isspace() else (p(x[1:], r=r, i=i + 1) and x[0] == r[i]) or (p(x[1:], r=r, i=i + 1) and x[0] == r[i].upper()) or (p(x[1:], r=r, i=i + 1) and x[0] == r[i].lower())
+
+
+p = is_palendrome
+
 if __name__ == "__main__":
     headers = {"Cookie": "session=" + str(open("sessioncookie").readline())}
     resp = requests.get("https://adventofcode.com/2020/day/2/input", headers=headers)
